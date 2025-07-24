0996daf844f34488425f686e25cdcefbb2a315cd
happymeatball-mk <mkucherenkode@gmail.com>
2025-07-24 20:24:21 +0300

    v.01

--- encmodule.js
+++ encmodule.js
@@ -1,15 +1,61 @@
-const { Readable } = require('node:stream');
+import { Readable, Transform, Writable } from 'node:stream';
 
-class Ui extends Readable {
+const toHex = string => Buffer.from(string, 'utf8').toString('hex')
 
-    constructor() {
-        super();
-        this.customers = new Map()
+export class Ui extends Readable {
+    constructor(data, opts = {}) {
+        super({ objectMode: true, ...opts });
+        this.queue = Array.isArray(data) ? [...data] : [data];
     }
 
     _read() {
-        
+        if (this.queue.length === 0) {
+            this.push(null);
+        } else {
+            this.push(this.queue.shift());
+        }
+    }
+}
+
+export class Guardian extends Transform {
+    constructor(opts = {}) {
+        super({ objectMode: true, ...opts })
     }
 
-};
+    _transform(chunk, encoding, callback) {
+        try {
+            const { email, password, ...rest } = chunk;
 
+            const secured = {
+                meta: { source: 'ui' },
+                payload: {
+                    ...rest,
+                    email:    email    ? toHex(email) : undefined,
+                    password: password ? toHex(password) : undefined,
+                },
+            };
+
+            this.push(secured);
+            callback();
+        } catch (err) {
+            callback(err);
+        }
+    }
+}
+
+export class AccountManager extends Writable {
+    constructor(opts = {}) {
+        super({ objectMode: true, ...opts });
+        this.accounts = [];
+    }
+
+    _write(chunk, encoding, callback) {
+        try {
+            this.accounts.push(chunk);
+            console.log(chunk.payload);
+            callback();
+        } catch (err) {
+            callback(err);
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+import {Ui, Guardian, AccountManager} from './encmodule.js';
+
+const customers = [
+    {
+        name: 'Pitter Black',
+        email: 'pblack@email.com',
+        password: 'pblack_123'
+    },
+    {
+        name: 'Oliver White',
+        email: 'owhite@email.com',
+        password: 'owhite_456'
+    }
+];
+
+const ui = new Ui(customers);
+const guardian = new Guardian();
+const manager = new AccountManager();
+ui.pipe(guardian).pipe(manager);
